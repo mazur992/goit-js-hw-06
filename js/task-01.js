96266ce5b8eab7568c73fb3254397277f525cdd5
Mazur Roman <superioritywork@gmail.com>
2023-02-19 11:24:26 +0100

    update 19.02

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const categories = document.querySelectorAll(".item");
+console.log("categories: ", categories);
+console.log("Number of categories:", categories.length);
+
+const cat1 = document.querySelector(".js-cat1");
+console.log("Category:", cat1.textContent);
+
+const cats1 = document.querySelectorAll(".js-cat1-item");
+console.log("Elements:", cats1.length);
+
+// ------------------------------------------------
+const cat2 = document.querySelector(".js-cat2");
+console.log("Category:", cat2.textContent);
+
+const cats2 = document.querySelectorAll(".js-cat2-item");
+console.log("Elements:", cats2.length);
+
+// ------------------------------------------------
+const cat3 = document.querySelector(".js-cat3");
+console.log("Category:", cat3.textContent);
+
+const cats3 = document.querySelectorAll(".js-cat3-item");
+console.log("Elements:", cats3.length);
