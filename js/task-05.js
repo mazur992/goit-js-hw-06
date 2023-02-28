0f738cc827b85e65560a128db0b92c2a7449b31c
Mazur Roman <superioritywork@gmail.com>
2023-02-28 21:13:59 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,8 @@ const textOutputEl = document.querySelector("#name-output");
 
 textInputEl.addEventListener("input", (event) => {
   textOutputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") textOutputEl.textContent = "Anonymous";
 });
 textInputEl.addEventListener("blur", (event) => {
-  if (event.currentTarget.value === "") {
-    textOutputEl.textContent = "Anonymous";
-  }
+  textInputEl.removeEventListener("input", () => {});
 });
